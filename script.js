734db573a23527282558296c17be903e12910472
Josh Rufin <joshrufin@gmail.com>
2017-08-08 15:34:52 -0400

    form resets when closed

--- script.js
+++ script.js
@@ -6,20 +6,17 @@ $(document).ready(function(){
 
 
   var reservations = [];
+  var person = {};
 
-  function makeRes () {
-    console.log('hi');
-  }
-
-  // Beg. of Click
+  // Open Modal
   $('.available').on('click', function (e){
-
-
     $('.modal').modal();
-
-
-      var person = {};
-
+  });
+  // Clears Form When Closed
+    $('#modal').on('hidden.bs.modal', function () {
+        $(this).find('form').trigger('reset');
+  });
+  // Reserve Seat
     $('.reserveButton').on('click', function (){
       person.name = $('#name').val();
       person.phone = $('#phone').val();
@@ -27,5 +24,4 @@ $(document).ready(function(){
       reservations.push(person);
       console.log(reservations);
     });
-  });
 });
