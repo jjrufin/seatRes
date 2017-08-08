cc10db5ba6cd023c7920bea376f61133d5ef44da
Josh Rufin <joshrufin@gmail.com>
2017-08-08 17:39:53 -0400

    change class works

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-  $('.seat').hover(function() {
+  $('.seat.available').hover(function() {
     $(this).toggleClass('seatHover');
   });
 
@@ -17,19 +17,27 @@ $(document).ready(function(){
   $('.available').on('click', function (e){
     var seatNum = e.target.innerText;
     var mySeat = e.target;
-    console.log(mySeat);
     $('.modal').modal();
     $('.reserveButton').on('click', function (){
       person.name = $('#name').val();
       person.phone = $('#phone').val();
       person.email = $('#email').val();
       person.seat = seatNum;
-      reservations.push(person);
+      reservations.unshift(person);
       console.log(reservations);
-    });
-    $('.seat.available').each(function(x, num){
-      console.log(x);
-      console.log(num);
+      $('.seat.available').each(function(x, num){
+        reservations.forEach(function(thisSeat){
+          if (num.innerText === thisSeat.seat){
+            console.log('hi');
+            $(num).removeClass('available').addClass('taken');
+          }
+        });
+      });
     });
   });
+  // End of Click
+
+
+
+
 });
