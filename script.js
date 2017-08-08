b4160004d95523df0c0dc005c2d34cdab62859ba
Josh Rufin <joshrufin@gmail.com>
2017-08-08 17:03:23 -0400

    bleh

--- script.js
+++ script.js
@@ -8,20 +8,28 @@ $(document).ready(function(){
   var reservations = [];
   var person = {};
 
-  // Open Modal
-  $('.available').on('click', function (e){
-    $('.modal').modal();
-  });
   // Clears Form When Closed
     $('#modal').on('hidden.bs.modal', function () {
         $(this).find('form').trigger('reset');
   });
-  // Reserve Seat
+
+  // Beginning of Click
+  $('.available').on('click', function (e){
+    var seatNum = e.target.innerText;
+    var mySeat = e.target;
+    console.log(mySeat);
+    $('.modal').modal();
     $('.reserveButton').on('click', function (){
       person.name = $('#name').val();
       person.phone = $('#phone').val();
       person.email = $('#email').val();
+      person.seat = seatNum;
       reservations.push(person);
       console.log(reservations);
     });
+    $('.seat.available').each(function(x, num){
+      console.log(x);
+      console.log(num);
+    });
+  });
 });
